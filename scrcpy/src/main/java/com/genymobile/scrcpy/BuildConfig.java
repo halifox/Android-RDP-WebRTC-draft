0e29fd32730ca397916a66e5a7c845cde90f76ec
halifox <106858061+halifox@users.noreply.github.com>
2025-02-07 09:58:23 +0800

    tmp

--- scrcpy/src/main/java/com/genymobile/scrcpy/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-package com.genymobile.scrcpy;
-
-public class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-    public static final String LIBRARY_PACKAGE_NAME = "com.genymobile.scrcpy";
-    public static final String BUILD_TYPE = "debug";
-    public static final String VERSION_NAME = "debug";
-}
