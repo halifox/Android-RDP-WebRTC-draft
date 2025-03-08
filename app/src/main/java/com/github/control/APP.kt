07ca5b1e01ccc501fa1bbb160f69cb8746baad20
halifox <106858061+halifox@users.noreply.github.com>
2025-03-08 12:35:30 +0800

    update

--- app/src/main/java/com/github/control/APP.kt
+++ app/src/main/java/com/github/control/APP.kt
@@ -1,6 +1,14 @@
 package com.github.control
 
 import android.app.Application
+import android.media.projection.MediaProjectionManager
+import android.net.nsd.NsdManager
+import androidx.core.content.ContextCompat
+import com.github.control.scrcpy.Controller
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 import org.webrtc.PeerConnectionFactory
 
 class APP : Application() {
@@ -10,5 +18,15 @@ class APP : Application() {
             PeerConnectionFactory.InitializationOptions.builder(applicationContext)
                 .createInitializationOptions()
         )
+
+        startKoin {
+            androidLogger()
+            androidContext(applicationContext)
+            modules(module {
+                single { ContextCompat.getSystemService(get(), NsdManager::class.java) }
+                single { ContextCompat.getSystemService(get(), MediaProjectionManager::class.java) }
+                single { Controller() }
+            })
+        }
     }
 }
--- app/src/main/java/com/github/control/ui/App.kt
+++ app/src/main/java/com/github/control/ui/App.kt
@@ -1,57 +1,37 @@
 package com.github.control.ui
 
-import android.media.projection.MediaProjectionManager
-import android.net.nsd.NsdManager
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.github.control.scrcpy.Controller
 import com.github.control.ui.theme.ControlTheme
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.compose.KoinApplication
-import org.koin.dsl.module
 
 
 val LocalNavController = compositionLocalOf<NavHostController> { error("No NavController provided.") }
 
 @Composable
 fun App() {
-    val context = LocalContext.current
-    KoinApplication(application = {
-        androidLogger()
-        androidContext(context.applicationContext)
-        modules(module {
-            single { ContextCompat.getSystemService(get(), NsdManager::class.java) }
-            single { ContextCompat.getSystemService(get(), MediaProjectionManager::class.java) }
-            single { Controller() }
-        })
-    }) {
-        ControlTheme {
-            val navController = rememberNavController()
-            CompositionLocalProvider(LocalNavController provides navController) {
-                NavHost(
-                    navController = navController,
-                    startDestination = "/HomeScreen",
-                ) {
-                    composable("/HomeScreen") {
-                        HomeScreen()
-                    }
-                    composable("/MasterScreen") {
-                        MasterScreen()
-                    }
-                    composable("/SlaveScreen") {
-                        SlaveScreen()
-                    }
+    ControlTheme {
+        val navController = rememberNavController()
+        CompositionLocalProvider(LocalNavController provides navController) {
+            NavHost(
+                navController = navController,
+                startDestination = "/HomeScreen",
+            ) {
+                composable("/HomeScreen") {
+                    HomeScreen()
                 }
-
+                composable("/MasterScreen") {
+                    MasterScreen()
+                }
+                composable("/SlaveScreen") {
+                    SlaveScreen()
+                }
             }
+
         }
     }
 }
