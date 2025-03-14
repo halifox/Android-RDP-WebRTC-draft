e40f8041a068aeb72af66dbee162c0e5bb546858
halifox <106858061+halifox@users.noreply.github.com>
2025-03-14 10:39:56 +0800

    update

--- app/src/main/java/com/github/control/ScreenCaptureServiceNettyImage.kt
+++ app/src/main/java/com/github/control/ScreenCaptureServiceNettyImage.kt
@@ -4,9 +4,9 @@ import android.graphics.Bitmap
 import android.graphics.PixelFormat
 import android.hardware.display.DisplayManager
 import android.hardware.display.VirtualDisplay
+import android.media.Image
 import android.media.ImageReader
 import android.media.projection.MediaProjection
-import android.os.Environment
 import android.os.HandlerThread
 import android.util.Log
 import androidx.lifecycle.DefaultLifecycleObserver
@@ -15,8 +15,7 @@ import com.blankj.utilcode.util.ScreenUtils
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelPipeline
 import io.netty.channel.SimpleChannelInboundHandler
-import java.io.File
-import java.io.FileOutputStream
+import java.io.ByteArrayOutputStream
 
 
 class ScreenCaptureServiceNettyImage : ScreenCaptureService0() {
@@ -25,61 +24,51 @@ class ScreenCaptureServiceNettyImage : ScreenCaptureService0() {
     private val handlerThread = HandlerThread("screenshot", 10).apply {
         start()
     }
+
     var count = 0
+    var lastSendTime = 0L
+    fun onImageAvailable0(image: Image) {
+        val planes = image.planes
+        val buffer = planes[0].buffer
+        val width = image.width
+        val height = image.height
+        val pixelStride = planes[0].pixelStride
+        val rowStride = planes[0].rowStride
+        val rowPadding = rowStride - pixelStride * width
+
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888)
+        bitmap.copyPixelsFromBuffer(buffer)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream)
+        val msg = byteArrayOutputStream.toByteArray()
+
+
+        Log.d("TAG", "writeAndFlush: ")
+        if (System.currentTimeMillis() > lastSendTime + 100) {
+            Log.d("TAG", "writeAndFlush:send[${count}] ${msg.size}")
+            if (ctx != null) {
+                count++
+            }
+            ctx?.writeAndFlush(msg)
+            lastSendTime = System.currentTimeMillis()
+        }
+
+
+
+        bitmap.recycle()
+    }
+
+    @Synchronized
+    fun onImageAvailable(reader: ImageReader) {
+        reader.acquireLatestImage()
+            ?.use {
+                onImageAvailable0(it)
+            }
+    }
+
     private val imageReader = ImageReader.newInstance(ScreenUtils.getScreenWidth(), ScreenUtils.getScreenHeight(), PixelFormat.RGBA_8888, 2)
         .apply {
-            val data = ByteArray((ScreenUtils.getScreenWidth() + 8) * ScreenUtils.getScreenHeight() * 4)
-
-            setOnImageAvailableListener({ reader ->
-
-                val img = reader.acquireLatestImage()
-                val planes = img.planes
-                val buffer = planes[0].buffer
-                if (buffer == null) return@setOnImageAvailableListener
-
-
-                val width = img.width
-                val height = img.height
-                val pixelStride = planes[0].pixelStride
-                val rowStride = planes[0].rowStride
-                val rowPadding = rowStride - pixelStride * width
-//                val newData = ByteArray(width * height * 4)
-
-                val bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888)
-                bitmap.copyPixelsFromBuffer(buffer)
-
-//                var offset = 0
-//                val bitmap = Bitmap.createBitmap(resources.displayMetrics, width, height, Bitmap.Config.ARGB_8888)
-//                val buffer = planes[0].buffer
-//                for (i in 0 until height) {
-//                    for (j in 0 until width) {
-//                        var pixel = 0
-//                        pixel = (pixel or (buffer.get(offset)
-//                            .toInt() and 0xff shl 16))   // R
-//                        pixel = (pixel or (buffer.get(offset + 1)
-//                            .toInt() and 0xff shl 8))  // G
-//                        pixel = (pixel or (buffer.get(offset + 2)
-//                            .toInt() and 0xff))       // B
-//                        pixel = (pixel or (buffer.get(offset + 3)
-//                            .toInt() and 0xff shl 24)) // A
-//                        bitmap.setPixel(j, i, pixel)
-//                        offset += pixelStride
-//                    }
-//                    offset += rowPadding
-//                }
-
-
-//                val name = "/myscreen$count.png"
-//                count++
-//                val file = File(cacheDir, name)
-//                val fos = FileOutputStream(file)
-//                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-//                Log.i("TAG", "image saved in ${Environment.getExternalStorageDirectory()}$name")
-//                fos.close()
-
-                bitmap.recycle()
-                img.close()
-            }, android.os.Handler(handlerThread.looper))
+            setOnImageAvailableListener(::onImageAvailable, android.os.Handler(handlerThread.looper))
         }
 
     var virtualDisplay: VirtualDisplay? = null
